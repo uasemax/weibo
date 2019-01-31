988a23dacf50e86df95a637da96d812d5f97b1f9
vagrant <vagrant@homestead>
2019-01-31 04:18:11 +0000

    登录成功后导航逻辑

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,7 +28,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
  * a simple convenience so we don't have to attach every token manually.
- */
+
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
@@ -37,6 +37,7 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+*/
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
