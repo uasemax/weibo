73a382ec8a8b33575895f7c12a73810154df0dc6
vagrant <vagrant@homestead>
2019-01-31 04:04:54 +0000

    创建会话

--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$db_config = get_db_config();
+
 return [
 
     /*
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'default' => $db_config('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
