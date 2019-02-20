ac5969b22dbf1ad68ef9c3f80016a6198a77283f
vagrant <vagrant@homestead>
2019-02-20 02:40:44 +0000

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,10 @@ class User extends Authenticatable
 	{
 		return $this->hasMany(Status::class);
     }
+
+	public function feed()
+	{
+		return $this->statuses()
+			->orderBy('created_at', 'desc');
+    }
 }
